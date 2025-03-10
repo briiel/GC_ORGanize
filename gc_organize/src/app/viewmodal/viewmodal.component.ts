9811af88ccede18b9aa550a0ce66e408cc0d0dc6
briiel <144537019+briiel@users.noreply.github.com>
2025-03-10 12:35:26 +0800

    Modified view modal.
    Made some UI changes with the View Modal.

--- gc_organize/src/app/viewmodal/viewmodal.component.ts
+++ gc_organize/src/app/viewmodal/viewmodal.component.ts
@@ -8,9 +8,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class ViewmodalComponent {
   @Output() close = new EventEmitter<void>();
+  imageUrl: string = '';
+
+  setImage(url: string) {
+    this.imageUrl = url;
+  }
 
   closeModal() {
     this.close.emit();
+  }
+
+  updateBackgroundImage(event: Event, bgElementId: string): void {
+    const imgElement = event.target as HTMLImageElement;
+    const bgElement = document.getElementById(bgElementId);
+    if (bgElement && imgElement) {
+      bgElement.style.backgroundImage = `url('${imgElement.src}')`;
+    }
   }
 }
 
