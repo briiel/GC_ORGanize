6162e05d22d2b90ace320fd95c0d91ccbdfad084
briiel <144537019+briiel@users.noreply.github.com>
2025-03-10 11:49:16 +0800

    Added file dropzone.
    Added drop zone for uploaded images. Made a functionality to add more images to the existing uploaded images.

--- gc_organize/src/app/registermodal/registermodal.component.ts
+++ gc_organize/src/app/registermodal/registermodal.component.ts
@@ -1,14 +1,43 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registermodal',
   templateUrl: './registermodal.component.html',
-  styleUrls: ['./registermodal.component.css']
+  styleUrls: ['./registermodal.component.css'],
+  imports: [CommonModule]
 })
 export class RegistermodalComponent {
   @Output() close = new EventEmitter<void>();
+  imageSrcs: string[] = [];
 
-  closeModal() {
+  previewImages(event: any) {
+    const files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imageSrcs.push(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  triggerFileInput() {
+    const fileInput = document.getElementById('hidden-file-input') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  removeImage(index: number) {
+    this.imageSrcs.splice(index, 1);
+  }
+
+  removeAllImages() {
+    this.imageSrcs = [];
+  }
+
+  closeModal(): void {
     this.close.emit();
   }
 }
