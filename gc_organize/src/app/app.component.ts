97f4e79705ef239458efcd392867ead1f1f4ac19
briiel <briangonzales0317@gmail.com>
2026-03-03 22:00:27 +0800

    Update app.component.ts
    - fix: add SwUpdate handler to auto-reload on new deployments

--- gc_organize/src/app/app.component.ts
+++ gc_organize/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routeAnimations } from './route-animations';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,25 @@ import { routeAnimations } from './route-animations';
 })
 export class AppComponent {
   title = 'gc_organize';
+
+  private swUpdate = inject(SwUpdate);
+
+  constructor() {
+    if (this.swUpdate.isEnabled) {
+      // When a new version of the app is ready, activate it and reload
+      this.swUpdate.versionUpdates
+        .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+        .subscribe(async () => {
+          await this.swUpdate.activateUpdate();
+          document.location.reload();
+        });
+
+      // Also check for updates every 5 minutes
+      setInterval(() => {
+        this.swUpdate.checkForUpdate();
+      }, 5 * 60 * 1000);
+    }
+  }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
